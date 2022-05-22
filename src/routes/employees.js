7a1379ea916722a1efd42f929a26cc0c0ef91507
andriusmw <andriusmw@yahoo.es>
2022-05-22 21:02:12 +0200

    obtener todos los datos de un usuario por id

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -5,6 +5,7 @@ const mysqlConnection = require("../database");
 
 router.get("/", (req, res) => {
   mysqlConnection.query("SELECT * FROM employees", (err, rows, fields) => {
+    //Lista todo lo que hay en la tabla employess, al llamar a esa ruta.
     if (!err) {
       res.json(rows);
     } else {
@@ -13,4 +14,19 @@ router.get("/", (req, res) => {
   });
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  mysqlConnection.query(
+    "SELECT * FROM employees WHERE id = ?",
+    [id],
+    (err, rows, fields) => {
+      if (!err) {
+        res.json(rows);
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
 module.exports = router;
