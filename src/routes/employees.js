ebecf8dc7cdf2da973b9faa86af4c12d35cf4e04
andriusmw <andriusmw@yahoo.es>
2022-05-22 20:40:57 +0200

    select all al cargar con get

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -1,4 +1,16 @@
 const express = require(`èxpress`);
 const router = express.router();
 
+const mysqlConnection = require("../database");
+
+router.get("/", (req, res) => {
+  mysqlConnection.query("SELECT * FROM employees", (err, rows, fields) => {
+    if (!err) {
+      res.json(rows);
+    } else {
+      console.log(err);
+    }
+  });
+});
+
 module.exports = router;
